656549b6db08b26ba45182540f7f09aa348abee1
unknown <krishna.n@campusuvce.in>
2025-12-16 21:46:37 -0800

    Add signup page before login

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Internships from "./pages/Internships";
 import Applications from "./pages/Applications";
+import Signup from "./pages/signup";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/internships" element={<Internships />} />
         <Route path="/applications" element={<Applications />} />
+        <Route path="/signup" element={<Signup/>} />
+
       </Routes>
     </BrowserRouter>
   );
