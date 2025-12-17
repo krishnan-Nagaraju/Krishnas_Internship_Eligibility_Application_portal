2d7eb2aa609a6b984d1fe9c8ca107403299dd3d6
unknown <krishna.n@campusuvce.in>
2025-12-17 00:41:03 -0800

    Fix signup route completely

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Internships from "./pages/Internships";
-import Applications from "./pages/Applications";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />  
-        <Route path="/internships" element={<Internships />} />
-        <Route path="/applications" element={<Applications />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
